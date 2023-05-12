2455e36a54c2899f91ea47a556a052582afe08a0
Niikogda <niikogda@gmail.com>
2023-05-12 20:40:42 +0300

    done)4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
+//#include <iostream>
+//#include <windows.h>
+//using namespace std;
+//int main()
+//{
+//	int grn;
+//	cout << "input grn: ";
+//	cin >> grn;
+//
+//	int cops;
+//	cout << "input cops: ";
+//	cin >> cops;
+//	
+//	cout << 
+//
+//}
+
 #include <iostream>
 #include <windows.h>
 using namespace std;
+
 int main()
 {
-	int grn;
-	cout << "input grn: ";
-	cin >> grn;
+    int hryvnia, kop;
+    cout << "Введіть суму в гривнях і копійках: ";
+    cin >> hryvnia >> kop;
 
-	int cops;
-	cout << "input cops: ";
-	cin >> cops;
+    int extraHryvnia = kop / 100;
+    int correctedKop = kop % 100;
+    int totalHryvnia = hryvnia + extraHryvnia;
 
+    cout << "Коректована сума: " << totalHryvnia << " грн " << correctedKop << " коп" << endl;
 
+    return 0;
 }

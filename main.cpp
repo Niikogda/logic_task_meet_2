848f3aab1b35124815eafc219e9ed9f5f3ff03b2
Niikogda <niikogda@gmail.com>
2023-05-12 20:35:04 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-//#include <iostream>
-//#include <windows.h>
-//using namespace std;
-//int main()
-//{
-//
-//}
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	int grn;
+	cout << "input grn: ";
+	cin >> grn;
+
+	int cops;
+	cout << "input cops: ";
+	cin >> cops;
+
+
+}

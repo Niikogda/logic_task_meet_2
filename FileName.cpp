fc69040f617ec7c8fb353122d459e7b7deb97f00
Laptop <Laptop@DESKTOP-BVI07RL>
2023-05-13 16:39:36 +0300

    do

---
+++ FileName.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+int main() {
+
+    double scale, distance_cm, distance_km;
+
+   
+    std::cout << "Обчислення відстані між населеними пунктами." << std::endl;
+    std::cout << "Введіть вихідні дані:" << std::endl;
+
+    std::cout << "Масштаб карти (кількість кілометрів в одному сантиметрі) -> ";
+    std::cin >> scale;
+
+    std::cout << "Відстань між точками, які зображують населені пункти (см) -> ";
+    std::cin >> distance_cm;
+
+    distance_km = distance_cm * scale / 100000.0;
+
+    std::cout << "Відстань між населеними пунктами: " << distance_km << " км." << std::endl;
+
+    return 0;
+}

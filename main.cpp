9705082d3dda976b04dbda5a8f6c1c531dc9bdbb
Laptop <Laptop@DESKTOP-BVI07RL>
2023-05-13 16:06:00 +0300

    dome

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <windows.h>
 namespase std()
 
 int main()
 {
-	int 
-	cout << ""
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Обчислення об’єму паралелепіпеда. "
+	int a;
+	cout << "Введіть висоту(см) -> ";
+	cin >> a;
+	int b;
+	cout << "введіть ширину(см) -> ";
+	cin >> b;
+	int c;
+	cout << "введіть довжину(см) -> ";
+	cin >> c;
+	cout << a*b*c
 }
